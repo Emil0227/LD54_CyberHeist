eeeb66c81ebc6843e1824635064d67f704ba55a4
emil0227 <emilcominghome@gmail.com>
2024-04-20 21:57:28 +0200

    git push

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 using UnityEngine.UI;
 using UnityEditor;
 
 public class GameManager : MonoBehaviour
 {
-    public Text TextScore;
+    [SerializeField] private Text m_TextScore;
+    [SerializeField] private GameObject m_BreakerReadyPrompt;
+    [SerializeField] private GameObject m_Bullet;
+    [SerializeField] private GameObject m_CDmask;
+    [SerializeField] private GameObject m_Player;
 
-    private void Awake()
+    private Timer m_CDTimer;
+    private bool m_IsGameStart;
+    private float m_BreakerCD;
+
+    void Start()
     {
         GameState.IsGamePaused = false;
         GameState.Score = 0;
+        m_CDmask.GetComponent<Image>().fillAmount = 0;
+        m_CDTimer = gameObject.AddComponent<Timer>();
+        m_IsGameStart = true;
+        m_BreakerCD = 3;
     }
+
     void Update()
     {
-        TextScore.text = "Score: " + GameState.Score.ToString();
+        m_TextScore.text = "Score: " + GameState.Score.ToString();
+        if (m_IsGameStart || m_CDTimer.Finished)//ready to shoot breaker
+        {
+            Shooting();
+        }
+        else
+        {
+            m_BreakerReadyPrompt.SetActive(false);
+        }
+    }
+
+    void Shooting()
+    {
+        m_BreakerReadyPrompt.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.Space))//shoot
+        {
+            m_IsGameStart = false;
+            //reset timer
+            m_CDTimer.Duration = m_BreakerCD;
+            m_CDTimer.Run();
+            //set countdown icon
+            m_BreakerReadyPrompt.SetActive(false);
+            m_CDmask.GetComponent<Image>().fillAmount = 1;
+            //release bullet
+            GameObject bullet = GameObject.Instantiate(m_Bullet);
+            bullet.transform.position = m_Player.transform.position;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,29 +8,20 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private GameObject m_LooseUI;
     [SerializeField] private GameObject m_WinUI;
-    [SerializeField] private GameObject m_Bullet;
-    [SerializeField] private GameObject m_CDmask;
-    [SerializeField] private GameObject m_BreakerReadyPrompt;
 
     private Rigidbody2D m_PlayerRigidbody;
     private float m_PlayerSpeedVertical;
     private float m_PlayerSpeedHorizontal;
-    private float m_BreakerCD;
     private Animator m_Anim;
-    private Timer m_CDTimer;
-    private bool m_IsGameStart;
+
     
 
     void Start()
     {
-        m_CDmask.GetComponent<Image>().fillAmount = 0;
         m_PlayerRigidbody = GetComponent<Rigidbody2D>();
         m_PlayerSpeedVertical = 5;
         m_PlayerSpeedHorizontal = 5;
-        m_BreakerCD = 3;
         m_Anim = GetComponent<Animator>();
-        m_CDTimer = gameObject.AddComponent<Timer>();
-        m_IsGameStart = true;
     }
 
     void FixedUpdate()
@@ -47,36 +38,6 @@ public class PlayerController : MonoBehaviour
         else
         {
             m_Anim.SetBool("isIdle", true);
-        }
-    }
-
-    void Update()
-    {
-        if (m_IsGameStart || m_CDTimer.Finished)//ready to shoot breaker
-        {
-            Shooting();
-        }
-        else
-        {
-            m_BreakerReadyPrompt.SetActive(false);
-        }
-    }
-
-    void Shooting()
-    {
-        m_BreakerReadyPrompt.SetActive(true);
-        if (Input.GetKeyDown(KeyCode.Space))//shoot
-        {
-            m_IsGameStart = false;
-            //reset timer
-            m_CDTimer.Duration = m_BreakerCD;
-            m_CDTimer.Run();
-            //set countdown icon
-            m_BreakerReadyPrompt.SetActive(false);
-            m_CDmask.GetComponent<Image>().fillAmount = 1;
-            //release bullet
-            GameObject bullet = GameObject.Instantiate(m_Bullet);
-            bullet.transform.position = transform.position;
         }
     }
 
